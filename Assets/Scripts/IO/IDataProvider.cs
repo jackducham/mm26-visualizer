1a99c6daecb515a2858d3ff8e2724fb049d32bbc
Zehua-Chen <peterchen06242000@outlook.com>
2019-11-16 17:47:42 -0600

    chore: Add Protol Buffer

--- Assets/Scripts/IO/IDataProvider.cs
+++ Assets/Scripts/IO/IDataProvider.cs
@@ -2,5 +2,14 @@ namespace MM26.IO
 {
     public interface IDataProvider
     {
+        VisualizerChange GetChange(int changeIndex);
+        VisualizerTurn GetTurn(int turnIndex);
+    }
+
+    public class DataProvider
+    {
+        public static IDataProvider ForDesktop() { return null; }
+        public static IDataProvider ForBrowser() { return null; }
+        public static IDataProvider Default() { return null; }
     }
 }
---
+++ Assets/Scripts/Scenes/WebsocketDesktop/ProtolBufferSceneController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.IO;
+using Google.Protobuf;
+using MM26.IO;
+
+namespace MM26.Scenes.WebsocketDesktop
+{
+    public class ProtolBufferSceneController : MonoBehaviour
+    {
+        MemoryStream stream = new MemoryStream();
+
+        void Awake()
+        {
+            VisualizerTurn turn = new VisualizerTurn
+            {
+                TurnNumber = 17,
+            };
+
+            turn.WriteTo(this.stream);
+
+            byte[] buffer = this.stream.GetBuffer();
+        }
+
+        void Start()
+        {
+            VisualizerTurn turn = VisualizerTurn.Parser.ParseFrom(this.stream);
+
+            Debug.Log(turn.TurnNumber);
+        }
+    }
+}
