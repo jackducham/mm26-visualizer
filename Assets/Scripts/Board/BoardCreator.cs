d81f05fea707201bc9ee6130bcc0575649ccd8e9
Zehua-Chen <peterchen06242000@outlook.com>
2020-07-06 13:34:06 -0500

    feat: Implement Simple Board Creator

--- Assets/Scripts/Board/BoardCreator.cs
+++ Assets/Scripts/Board/BoardCreator.cs
@@ -3,7 +3,8 @@ using UnityEngine.Tilemaps;
 
 namespace MM26.Board
 {
-    using PBoard = MM26.IO.Models.Board;
+    using PTileType = MM26.IO.Models.Tile.Types.TileType;
+    using PTile = MM26.IO.Models.Tile;
 
     public class BoardCreator : MonoBehaviour
     {
@@ -12,7 +13,16 @@ namespace MM26.Board
 
         [Header("Tiles")]
         [SerializeField]
-        Tile _tile = null;
+        Tile _voidTile = null;
+
+        [SerializeField]
+        Tile _portalTile = null;
+
+        [SerializeField]
+        Tile _impassibleTile = null;
+
+        [SerializeField]
+        Tile _blankTile = null;
 
         [Header("Services")]
         [SerializeField]
@@ -40,7 +50,38 @@ namespace MM26.Board
             var board = _data.GameState.BoardNames[_board];
 
             // TODO: set tils here!
-            _tilemap.SetTile(new Vector3Int(), _tile);
+            for (int y = 0; y < board.Rows; y++)
+            {
+                for (int x = 0; x < board.Columns; x++)
+                {
+                    PTile ptile = board.Grid[y * board.Rows + x];
+                    Tile tile = null;
+
+                    switch (ptile.TileType)
+                    {
+                        case PTileType.Void:
+                            tile = _voidTile;
+                            break;
+                        case PTileType.Portal:
+                            tile = _portalTile;
+                            break;
+                        case PTileType.Impassible:
+                            tile = _impassibleTile;
+                            break;
+                        case PTileType.Blank:
+                            tile = _blankTile;
+                            break;
+                    }
+
+                    if (tile == null)
+                    {
+                        Debug.LogError("tile cannot be null!");
+                        continue;
+                    }
+
+                    _tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                }
+            }
 
             _sceneLifeCycle.BoardCreated.Invoke();
         }
