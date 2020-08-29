3e1334bbaf862e486a686e488728429fd3e2199d
Zehua-Chen <peterchen06242000@outlook.com>
2020-08-29 15:46:45 -0500

    feat: Add Director Component

--- Assets/Scripts/Board/BoardPositionLookUp.cs
+++ Assets/Scripts/Board/BoardPositionLookUp.cs
@@ -1,15 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Tilemaps;
 
 namespace MM26.Board
 {
+    /// <summary>
+    /// Allows other components to perform position lookup in the tile map
+    /// </summary>
     [CreateAssetMenu(fileName = "BoardPositionLookUp", menuName = "Board/Board Position Lookup")]
     public class BoardPositionLookUp : ScriptableObject
     {
         public Tilemap Tilemap { get; set; } = null;
 
+        /// <summary>
+        /// Given a tilemap position, translate to game position
+        /// </summary>
+        /// <param name="position">tilemap position</param>
+        /// <returns>game position</returns>
         public Vector3 Translate(Vector3Int position)
         {
             return this.Tilemap.GetCellCenterWorld(position);
---
+++ Assets/Scripts/Play/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace MM26.Play
+{
+    /// <summary>
+    /// Responsible for playing the scene from deltas
+    /// </summary>
+    public class Player : MonoBehaviour
+    {
+        [Header("Services")]
+        [SerializeField]
+        private SceneLifeCycle _sceneLifeCycle = null;
+
+        [SerializeField]
+        private MM26.IO.Data _data = null;
+    }
+}
