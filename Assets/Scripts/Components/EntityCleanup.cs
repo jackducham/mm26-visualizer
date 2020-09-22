d5603ee8b14b97a17734475fd9ae1d59b80dca4f
Zehua-Chen <peterchen06242000@outlook.com>
2020-09-21 23:59:25 -0500

    fix: Fix Errors with Despawn Task

---
+++ Assets/Scripts/Components/EntityCleanup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+namespace MM26.Components
+{
+    /// <summary>
+    /// When the game object is destroyed, also destroys the entity (ECS)
+    /// associated with the game object
+    /// </summary>
+    public class EntityCleanup : MonoBehaviour, IConvertGameObjectToEntity
+    {
+        private Entity _entity = default;
+        private World _world = null;
+
+        public void Convert(
+            Entity entity,
+            EntityManager dstManager,
+            GameObjectConversionSystem conversionSystem)
+        {
+            _entity = entity;
+            _world = dstManager.World;
+        }
+
+        private void OnDestroy()
+        {
+            _world.EntityManager.DestroyEntity(_entity);
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/Scenes/DespawnScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using MM26.ECS;
+using MM26.Tasks;
+
+namespace MM26.Tests.Scenes
+{
+    public class DespawnScene : MonoBehaviour
+    {
+        [SerializeField]
+        Mailbox _mailbox = null;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _mailbox.SendTask(new SpawnPlayerTask("player", new Vector3Int()));
+            StartCoroutine(this.Destroy());
+        }
+
+        private IEnumerator Destroy()
+        {
+            yield return new WaitForSecondsRealtime(1.0f);
+            _mailbox.SendTask(new DespawnTask("player"));
+        }
+    }
+}
