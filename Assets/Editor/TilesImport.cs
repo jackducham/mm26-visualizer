5e73faf873809af1224bf5de470fa5f31abc6dd0
Zehua-Chen <peterchen06242000@outlook.com>
2020-09-23 16:14:15 -0500

    feat: Reimport Assets

---
+++ Assets/Editor/TilesImport.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
+
+public class TilesImport
+{
+
+    [MenuItem("Assets/Import Tiles")]
+    public static void ImportTiles()
+    {
+        var tiles = Directory.EnumerateFiles("Assets/Sprites/mm26_tiles/raw")
+            .Where(path => Path.GetExtension(path) != ".meta")
+            .Select(path =>
+            {
+                string name = Path.GetFileNameWithoutExtension(path);
+                var sprite = AssetDatabase.LoadAssetAtPath<Sprite>(path);
+
+                return (name, sprite);
+            })
+            .Select(item =>
+            {
+                var tile = ScriptableObject.CreateInstance<Tile>();
+                tile.sprite = item.sprite;
+
+                return (tile, item.name);
+            });
+
+        foreach (var tile in tiles)
+        {
+            string path = Path.Combine("Assets/Sprites/mm26_tiles", $"{tile.name}.asset");
+
+            AssetDatabase.CreateAsset(tile.tile, path);
+
+            Debug.Log(path);
+        }
+
+        AssetDatabase.SaveAssets();
+    }
+}
