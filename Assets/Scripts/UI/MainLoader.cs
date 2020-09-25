0a4b547fbec681e294114976103f06098391a976
Zehua-Chen <peterchen06242000@outlook.com>
2020-09-25 18:55:03 -0500

    feat: Add Server Selection

--- Assets/Scripts/UI/MainLoader.cs
+++ Assets/Scripts/UI/MainLoader.cs
@@ -17,23 +17,42 @@ namespace MM26.UI
         private string _sceneName = "Main";
 
         [SerializeField]
-        TMP_InputField _urlField = null;
+        TMP_InputField _boardNameField = null;
 
         [SerializeField]
-        TMP_InputField _boardNameField = null;
+        TextMeshProUGUI _serverLabel = null;
+
+        string _url = "ws://engine-main.mechmania.io:8081/visualizer";
 
         private void Start()
         {
-            _urlField.text = "ws://engine-main.mechmania.io:8081/visualizer";
             _boardNameField.text = "pvp";
 
             _sceneConfiguration.WebSocketURL = "ws://engine-main.mechmania.io:8081/visualizer";
             _sceneConfiguration.BoardName = "pvp";
         }
 
+        public void OnUseMainServer()
+        {
+            _serverLabel.text = "Server: Main";
+            _url = "ws://engine-main.mechmania.io:8081/visualizer";
+        }
+
+        public void OnUseTestServer()
+        {
+            _serverLabel.text = "Server: Test";
+            _url = "ws://engine-test.mechmania.io:8081/visualizer";
+        }
+
+        public void OnUseLocalHost()
+        {
+            _serverLabel.text = "Server: Local";
+            _url = "ws://localhost:8081/visualizer";
+        }
+
         public void OnLoadClick()
         {
-            _sceneConfiguration.WebSocketURL = _urlField.text.KeepVisibles();
+            _sceneConfiguration.WebSocketURL = _url;
             _sceneConfiguration.BoardName = _boardNameField.text.KeepVisibles();
 
             SceneManager.LoadScene(_sceneName);
