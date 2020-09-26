6b18deb482dd6cd42839854c2d2f0988464162ad
unknown <animating101@gmail.com>
2020-09-26 14:50:24 -0500

    v0.7: Fixed overlapping UI, and misplaced treasure troves

--- Assets/Scripts/Board/TreasureTrovesManager.cs
+++ Assets/Scripts/Board/TreasureTrovesManager.cs
@@ -11,31 +11,17 @@ namespace MM26.Board
         [SerializeField]
         private Tilemap _tilemap = null;
 
-<<<<<<< HEAD
-        public int mapHeight = 0;
-
-        public void PlaceTrove(int x, int y)
-        {
-            Debug.Log(_tilemap.cellSize);
-            Debug.Log(_tilemap.name);
-            _tilemap.SetTile(new Vector3Int(x, (mapHeight - 1) - y, 0), _troveTile);
-=======
         [SerializeField]
         private BoardPositionLookUp _boardPositionLookUp = null;
 
         public void PlaceTrove(int x, int y)
         {
             _tilemap.SetTile(new Vector3Int(x, (_boardPositionLookUp.Height - 1) - y, 0), _troveTile);
->>>>>>> 42f5e6b1eec72c193dc6959baaec1893579e88a8
         }
 
         public void RemoveTrove(int x, int y)
         {
-<<<<<<< HEAD
-            _tilemap.SetTile(new Vector3Int(x, (mapHeight - 1) - y, 0), null);
-=======
             _tilemap.SetTile(new Vector3Int(x, (_boardPositionLookUp.Height - 1) - y, 0), null);
->>>>>>> 42f5e6b1eec72c193dc6959baaec1893579e88a8
         }
     }
 }
